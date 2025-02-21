2b07193981a617fade8536b03c4d65673d5fb3ef
Jakub Ježek <jezekj22@spstrutnov.cz>
2025-02-21 12:07:19 +0100

    server vypíše webovou stránku

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
-
-const cors = require("cors");
+const path = require('path');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
+
 app.use(express.json());
-app.use(cors());
 
 module.exports = app;
