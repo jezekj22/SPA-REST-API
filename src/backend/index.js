dbe38e52e2adb796478726a5fbc9e5901b67cb6c
Martin Forbelský <mforbelak@gmail.com>
2025-03-26 17:28:59 +0100

    registrace

--- src/backend/index.js
+++ src/backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
 const path = require('path');
-
+const JSONdb = require('simple-json-db');
 
 const app = express();
 app.use(express.json());
@@ -33,15 +33,105 @@ app.get('/random-quote', (req, res) => {
 });
 
 
+class User {
+    /**
+     * Constructor method
+     * @param { int } id 
+     * @param { string } username 
+     * @param { string } password 
+     * @param { string } displayName
+     * @param { int } generatedQuotes
+     * @param { int } favoritedQuotes
+     */  
+    constructor(id, username, password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.db = new JSONdb(path.join(__dirname, '../data/users.json'));
+        
+        if (!this.db.has('next_id')) {
+            this.db.set('next_id', 1);
+        }
+
+        if (!this.db.has('users')) {
+            this.db.set('users', []);
+        }
+    }
+        
+    getNextId() {
+        return this.db.get('next_id');
+    }
+
+    FindUserbyName(username){
+        let users = this.db.get('users') || [];
+        let userData = users.find(user => user.username === username); 
+        console.log(username)
+        if(!userData){
+            return false
+        }
+        return  new User(userData.id, userData.username, userData.password); 
+    }
+
+    create(user) {
+        const users = this.db.get('users') || [];
+        if(this.FindUserbyName(user.username)){
+            throw new Error("User already exists");
+ 
+        }
+               
+        users.push({
+            id: this.getNextId(),
+            username: user.username,
+            password: user.password,
+            displayName: user.displayName,
+            generatedQuotes: user.generatedQuotes,
+            favoritedQuotes: user.favoritedQuotes
+        });
+    
+        this.db.set('users', users);
+        this.db.set('next_id', this.getNextId() + 1);
+        this.db.sync();
+        return user;
+    }
+}
+
+
+
+
 
 app.post('/api/login',(req,res) =>{
     const { name, passwd } = req.body;
     console.log(name + " " + passwd);
 });
 
-app.post('/api/register',(req,res) =>{
+app.post('/api/register', (req, res) => {
     const { name, passwd, checkpasswd } = req.body;
-    console.log(name + " " + passwd + " " + checkpasswd);
+
+    if (!name || !passwd || !checkpasswd) {
+        return res.status(400).json({ error: "All fields are required" });
+    }
+
+    if (passwd !== checkpasswd) {
+        return res.status(400).json({ error: "Passwords do not match" });
+    }
+
+    try {
+        const userDb = new User(); 
+        if (userDb.FindUserbyName(name)) {
+            return res.status(400).json({ error: "User already exists" });
+        }
+
+        const newUser = userDb.create({
+            username: name,
+            password: passwd
+        });
+
+        return res.status(201).json({ message: "User registered successfully", user: newUser });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
 });
 
 
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const backend = require('./backend');
 
 
-http.createServer(backend).listen(8080, () => {
-    console.log('Server runs on: http://localhost:8080');
+http.createServer(backend).listen(8000, () => {
+    console.log('Server runs on: http://localhost:8000');
 });
 
